e8f2043f1a94fe06c52c745d856ba34b890fdb1c
Eray Hanoğlu <e.hanoglu@panates.com>
2023-06-21 11:17:03 +0300

    Configure custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/logoDark.svg',
 
   // Set the production url of your site here
-  url: 'https://sqbjs.github.io',
+  url: 'https://sqbjs.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sqbdocs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

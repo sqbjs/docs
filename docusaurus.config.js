0a48360882856ed5589534ec1fc67cc78ef32ef7
Eray Hanoğlu <e.hanoglu@panates.com>
2023-06-21 10:56:29 +0300

    Initial test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://sqbjs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/sqbdocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -101,7 +101,7 @@ const config = {
           style: 'dark',
           links: [
             {
-              title: 'Docs',
+              title: 'Documents',
               items: [
                 {
                   label: 'Tutorial',
